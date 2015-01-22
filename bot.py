5919ef031fadbe7e3a3b4e14bfa602ae18dbcab7
Matt Smith <matt.the.smith@gmail.com>
2015-01-21 23:23:58 -0800

    multiple recipients and matching for make it rain

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ class SlackBot(BaseBot):
 
     channel = "slack"
     changetip_api_key = CHANGETIP_API_KEY
+    wrappers = ('(',')')
 
     def unique_id(self, post_data):
         # Generatee a special id to prevent duplicates.
@@ -23,3 +24,33 @@ class SlackBot(BaseBot):
         checksum.update(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:00').encode("utf8"))
 
         return checksum.hexdigest()[:16]
+
+
+    def get_mentions(self, message):
+        """ Remove duplicates in a case-insensitive way while preserving the original order
+        Return all mentions in lower case *without* their prefixes. (So return ['clyde'], not ['@clyde'])
+        >>> BaseBot().get_mentions("This is a @user")
+        ['user']
+        >>> BaseBot().get_mentions("This is empty")
+        []
+        >>> BaseBot().get_mentions("title case and end of string @mention @ChangeTip. and @")
+        ['mention', 'changetip']
+        >>> BaseBot().get_mentions("@ This one has an empty one and two @mention-69 @changetip.")
+        ['mention-69', 'changetip']
+        >>> BaseBot().get_mentions("This one has a dupe @mention-69 @changetip and @mention-69.")
+        ['mention-69', 'changetip']
+        >>> SlackBot().get_mentions("This one looks like <@postdata> <@usernames>")
+        ['postdata', 'usernames']
+        """
+        mentions = re.findall(self.wrappers[0] + re.escape(self.prefix) + '([\w-]+)' + self.wrappers[1],
+                              message)
+        mentions_set = set([m.lower() for m in mentions])
+
+        deduped_mentions = []
+        for m in mentions:
+            m = m.lower()
+            if m in mentions_set:
+                mentions_set.remove(m)
+                deduped_mentions.append(m)
+
+        return deduped_mentions
--- slack/views.py
+++ slack/views.py
@@ -15,6 +15,7 @@ def command_webhook(request):
     info_url = "https://www.changetip.com/tip-online/slack"
     get_started = "To send your first tip, login with your slack account on ChangeTip: %s" % info_url
     print(json.dumps(request.POST.copy(), indent=2))
+
     # Do we have this user?
     user_name = request.POST.get("user_name")
     slack_sender, created = SlackUser.objects.get_or_create(
@@ -27,54 +28,67 @@ def command_webhook(request):
 
     text = request.POST.get("text", "")
 
-    # Check for mention in the format of <@$userid>
-    mention_match = re.search('<@(U[A-Z0-9]+)>', text)
-    if not mention_match:
+    # Check for mentions in the format of <@$userid>
+    bot = SlackBot()
+    mention_matches = bot.get_mentions(text)
+    if not mention_matches:
         # Say something clever
         cb = cleverbot.Cleverbot()
         response = cb.ask(text.replace('changetip', ''))
         return JsonResponse({"text": response})
 
-    slack_receiver = SlackUser.objects.filter(team_id = slack_sender.team_id, user_id=mention_match.group(1)).first()
-    if not slack_receiver:
+    slack_receivers = SlackUser.objects.filter(
+        team_id = slack_sender.team_id,
+        user_id__in=[m.group(1) for m in mention_matches]
+    )
+
+    if not slack_receivers:
         return JsonResponse({"text": "%s, I don't know who that person is yet. They should say hi to me before I give them money." % user_name})
 
-    # Substitute the @username back in
-    text = text.replace(mention_match.group(0), '@%s' % slack_receiver.name)
+    # Check for requests to make it rain
+    if re.search(r"make it ra+i+n", text, re.IGNORECASE):
+        recipient_limit = 50 # Sane?
+    else:
+        recipient_limit = 1
 
-    # Submit the tip
-    bot = SlackBot()
+    # Submit the tips
     team_domain = request.POST.get("team_domain")
-    tip_data = {
-        "sender": "%s@%s" % (slack_sender.name, team_domain),
-        "receiver": "%s@%s" % (slack_receiver.name, team_domain),
-        "message": text,
-        "context_uid": bot.unique_id(request.POST.copy()),
-        "meta": {}
-    }
-    for meta_field in ["token", "team_id", "channel_id", "channel_name", "user_id", "user_name", "command"]:
-        tip_data["meta"][meta_field] = request.POST.get(meta_field)
 
-    if request.POST.get("noop"):
-        return JsonResponse({"text": "Hi!"})
+        out = ""
+    for i, receiver in enumerate(slack_receivers):
+        if i >= recipient_limit:
+            break
+        tip_data = {
+            "sender": "%s@%s" % (slack_sender.namze, team_domain),
+            "receiver": "%s@%s" % (receiver.name, team_domain),
+            "message": text,
+            "context_uid": bot.unique_id(request.POST.copy()),
+            "meta": {}
+        }
+        for meta_field in ["token", "team_id", "channel_id", "channel_name", "user_id", "user_name", "command"]:
+            tip_data["meta"][meta_field] = request.POST.get(meta_field)
 
-    response = bot.send_tip(**tip_data)
-    out = ""
-    if response.get("error_code") == "invalid_sender":
-        out = get_started
-    elif response.get("error_code") == "duplicate_context_uid":
-        out = "That looks like a duplicate tip."
-    elif response.get("error_message"):
-        out = response.get("error_message")
-    elif response.get("state") in ["ok", "accepted"]:
-        tip = response["tip"]
-        if tip["status"] == "out for delivery":
-            out += "The tip for %s is out for delivery. %s needs to collect by connecting their ChangeTip account to slack at %s" % (tip["amount_display"], tip["receiver"], info_url)
-        elif tip["status"] == "finished":
-            out += "The tip has been delivered, %s has been added to %s's ChangeTip wallet." % (tip["amount_display"], tip["receiver"])
+        if request.POST.get("noop"):
+            return JsonResponse({"text": "Hi!"})
 
-    if "+debug" in text:
-        out += "\n```\n%s\n```" % json.dumps(response, indent=2)
+        response = bot.send_tip(**tip_data)
+        if out:
+            out += "\n"
+        if response.get("error_code") == "invalid_sender":
+            out = get_started
+        elif response.get("error_code") == "duplicate_context_uid":
+            out = "That looks like a duplicate tip."
+        elif response.get("error_message"):
+            out = response.get("error_message")
+        elif response.get("state") in ["ok", "accepted"]:
+            tip = response["tip"]
+            if tip["status"] == "out for delivery":
+                out += "The tip for %s is out for delivery. %s needs to collect by connecting their ChangeTip account to slack at %s" % (tip["amount_display"], tip["receiver"], info_url)
+            elif tip["status"] == "finished":
+                out += "The tip has been delivered, %s has been added to %s's ChangeTip wallet." % (tip["amount_display"], tip["receiver"])
+
+        if "+debug" in text:
+            out += "\n```\n%s\n```" % json.dumps(response, indent=2)
 
     return JsonResponse({"text": out})
 
